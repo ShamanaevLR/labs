da58ea8ec6f4ee93a45df6ed2bd3c4d2a6442e0b
ShamanaevLR <72642558+ShamanaevLR@users.noreply.github.com>
2020-10-17 11:12:13 +0700

    Update zadanie2.c

--- zadanie2.c
+++ zadanie2.c
@@ -5,24 +5,24 @@ int is_simple(int number)
 {
    if (number < 2)
    {
-    printf("Wrong Number");
+      printf("Wrong Number");
       
-    return 2;
+      return 2;
    }
-      for (int i = 2; i < number; i++)
-      {
-      if (number % i == 0)
-      {
-      return 0;
-      }
+   for (int i = 2; i < number; i++)
+   {
+       if (number % i == 0)
+       {
+          return 0;
+       }
    }
    return 1;
 }
 int main(void)
 {
- int num;
- scanf("%d", &num);
- printf("%d", is_simple(num));
+    int num;
+    scanf("%d", &num);
+    printf("%d", is_simple(num));
 
-return 0;
+    return 0;
 }
