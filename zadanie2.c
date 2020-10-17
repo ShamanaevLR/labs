98a8ce8690af4b8e4c7f73fd0263a9a1f806f559
ShamanaevLR <72642558+ShamanaevLR@users.noreply.github.com>
2020-10-17 10:53:46 +0700

    Update zadanie2.c

--- zadanie2.c
+++ zadanie2.c
@@ -10,20 +10,20 @@ int is_simple(int number)
       
     return 2;
    }
-   for (int i = 2; i < number; i++)
-   {
-     if (number % i == 0)
-     {
+      for (int i = 2; i < number; i++)
+      {
+      if (number % i == 0)
+      {
       return 0;
-     }
+      }
    }
-    return 1;
+   return 1;
 }
 int main(void)
 {
-    int num;
-    scanf("%d", &num);
-    printf("%d", is_simple(num));
+ int num;
+ scanf("%d", &num);
+ printf("%d", is_simple(num));
 
-    return 0;
+return 0;
 }
