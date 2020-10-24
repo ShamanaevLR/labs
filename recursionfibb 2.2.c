545431e0b2f6f8c21b64a1456948d97078ecb981
ShamanaevLR <72642558+ShamanaevLR@users.noreply.github.com>
2020-10-24 13:28:45 +0700

    Update recursionfibb 2.2.c

--- recursionfibb 2.2.c
+++ recursionfibb 2.2.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
 int cache[300];
-
 long long get_fibbonachi_via_recursion(int number)
 {
     if (cache[number] == 0)
     {
         if (number == 1 || number == 2)
         {
-          cache[number] = 1;
+            cache[number] = 1;
         }
         else
         {
-          cache[number] = get_fibbonachi_via_recursion(number - 1) + get_fibbonachi_via_recursion(number - 2);
+            cache[number] = get_fibbonachi_via_recursion(number - 1) + get_fibbonachi_via_recursion(number - 2);
         }
     }
 
-    return cache[number];
+return cache[number];
 }
 
 int main()
@@ -29,5 +28,5 @@ int main()
         printf("%llu\t\n", get_fibbonachi_via_recursion(count));
       }
       
-    return 0;
+return 0;
 }
