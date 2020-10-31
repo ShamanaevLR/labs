47f1ab541855e8ec3557096e66ce4fe8ce79a125
ShamanaevLR <72642558+ShamanaevLR@users.noreply.github.com>
2020-10-31 11:28:02 +0700

    Update cyclefibb 2.1.c

--- cyclefibb 2.1.c
+++ cyclefibb 2.1.c
@@ -13,11 +13,11 @@ long long get_fibbonachi_via_cycle(int number)
 {
   long fib = 1;
   long fib_sum = 1;
-    for (int i = 0; i < number - 2; i++)
-    {
-      fib += fib_sum;
-      fib_sum = fib - fib_sum;
-    }
+  for (int i = 0; i < number - 2; i++)
+  {
+    fib += fib_sum;
+    fib_sum = fib - fib_sum;
+  }
 
   return fib_sum;
 }
