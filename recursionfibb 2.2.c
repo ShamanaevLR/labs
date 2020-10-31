a1d463d650fa4151a42510a7793068420a663f09
ShamanaevLR <72642558+ShamanaevLR@users.noreply.github.com>
2020-10-31 12:38:15 +0700

    Update recursionfibb 2.2.c

--- recursionfibb 2.2.c
+++ recursionfibb 2.2.c
@@ -2,6 +2,21 @@
 
 int cache[300];
 
+long long get_fibbonachi_via_recursion(int number);
+
+int main()
+{
+int n;
+printf("Enter n for find fibbonachi: ");
+scanf("%d", &n);
+for (int count = 1; count < n; count++)
+    {
+    printf("%llu\t\n", get_fibbonachi_via_recursion(count));
+    }
+      
+    return 0;
+}
+
 long long get_fibbonachi_via_recursion(int number)
 {
     if (cache[number] == 0)
@@ -17,17 +32,4 @@ long long get_fibbonachi_via_recursion(int number)
     }
 
     return cache[number];
-}
-
-int main()
-{
-int n;
-printf("Enter n for find fibbonachi: ");
-scanf("%d", &n);
-for (int count = 1; count < n; count++)
-    {
-    printf("%llu\t\n", get_fibbonachi_via_recursion(count));
-    }
-      
-    return 0;
 }
